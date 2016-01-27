35bc4204d4b75c578293f20b36b1740a534fb158
Jorge Zapata <jorge.zapata@jazg.net>
2016-01-27 00:40:40 -0600

    Setting up phpunit, composer, namespaces and final directory structure

---
+++ Contests/ThirtyDaysOfCode/Day25/Test/SolutionTest.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Jorge
+ * Date: 1/26/16
+ * Time: 11:17 PM
+ */
+
+namespace HackerRank\Contests\ThirtyDaysOfCode\Day25\Test;
+
+use HackerRank\Contests\ThirtyDaysOfCode\Day25\Solution;
+
+class SolutionTest extends \PHPUnit_Framework_TestCase
+{
+    protected $solution;
+
+    public function setup()
+    {
+        $this->solution = new Solution();
+    }
+
+    public function primeNumbers()
+    {
+        return [
+            [5],
+            [7],
+            [7237],
+            [7919],
+            [7907],
+            [7883],
+            [902501],
+            [999983]
+        ];
+    }
+
+    public function notPrimeNumbers()
+    {
+        return [
+            [0],
+            [1],
+            [30],
+            [35],
+            [70]
+        ];
+    }
+
+    /**
+     * @dataProvider primeNumbers
+     */
+    public function testNumbersArePrime($n)
+    {
+        $this->assertTrue($this->solution->isPrime($n));
+    }
+
+    /**
+     * @dataProvider notPrimeNumbers
+     */
+    public function testNumbersAreNotPrime($n)
+    {
+        $this->assertFalse($this->solution->isPrime($n));
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,6 @@
+#Welcome
+
+This repository consists of some solutions to challenges proposed in popular site Hacker Rank.
+
+Feel free to clone this repository, discuss or propose other solution (in this case I suggest forking the repo) or even 
+make a pull request if you find missing tests or the solution is wrong.
