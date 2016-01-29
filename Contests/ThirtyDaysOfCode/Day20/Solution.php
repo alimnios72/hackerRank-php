070601da0b5ff5029a65153df64afc44884ceb2f
Jorge Zapata <jorge.zapata@jazg.net>
2016-01-28 22:31:45 -0600

    Solution to challenge 20 with unit tests

--- Contests/ThirtyDaysOfCode/Day20/Solution.php
+++ Contests/ThirtyDaysOfCode/Day20/Solution.php
@@ -1,8 +1,22 @@
 <?php
-$s = "a[ ";
+namespace HackerRank\Contests\ThirtyDaysOfCode\Day20;
+
+class Solution 
+{
+    protected $pattern = "/[\s\!\[,\?\.\\\_'@\+\]]+/";
+
+    public function wordSplit($string)
+    {
+        return preg_split($this->pattern, $string, -1, PREG_SPLIT_NO_EMPTY);
+    }
+}
+
+//$s = "He is a very very good boy, isn't he?";
 
 
-$words = preg_split("/[\s\!\[,\?\.\_'@\+\]]+/", $s, -1, PREG_SPLIT_NO_EMPTY);
+//$words = preg_split("/[\s\!\[,\?\.\_'@\+\]]+/", $s, -1, PREG_SPLIT_NO_EMPTY);
+/*$solution = new Solution();
+$words = $solution->wordSplit($s);
 echo count($words)."\n";
-echo implode("\n", $words)."\n";
+echo implode("\n", $words)."\n";*/
 ?>
